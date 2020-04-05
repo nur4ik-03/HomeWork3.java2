61415d274b0f115bb24d28b058072b7a67fcf0a8
nur4ik-03 <4u.lp@bk.ru>
2020-04-05 17:05:43 +0600

    new

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -1,28 +1,32 @@
 package com.company;
 
 public class BankAccount {
-    double amount;
+  private double amount;
 
     public BankAccount(double amount) {
         this.amount = amount;
     }
+
     public BankAccount(){}
 
     public double getAmount() {
         return amount;
     }
-    public int withDraw(int sum) throws LimitException {
-        amount = amount - sum;
-        System.out.println("C вашего счёта снято  = " + sum);
-        if (sum > amount) {
+    public void withDraw(int sum) throws LimitException {
+        if (getAmount() < sum) {
             throw new LimitException("У Вас недостаточно средств", getAmount());
         }
-        return sum;
+        amount = amount - sum;
+        System.out.println("C вашего счёта снято  = " + sum);
+
+
     }
 
     public double deposit(double sum){
+        if (sum > 0){
         amount = amount + sum;
-        System.out.println("На ваш счёт зачислено = " + sum);
+        System.out.println("На ваш счёт зачислено = " + amount);
+        }
         return sum;
     }
 }
