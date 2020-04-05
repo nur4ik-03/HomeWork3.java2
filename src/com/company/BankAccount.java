24f14bade326154d886e590063a8af60613b5359
nur4ik-03 <4u.lp@bk.ru>
2020-04-05 21:13:21 +0600

    peredelal

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -14,13 +14,23 @@ public class BankAccount {
     }
     public void withDraw(int sum) throws LimitException {
         if (getAmount() < sum) {
+            System.out.println("Вы можете снять только "  + amount);
+            amount = amount - amount;
+            System.out.println("Остаток на счёта " + amount);
+            throw new LimitException("Недостаточно средств на балансе. Остаток средств на счёта    ", getAmount());
+        } else {
+            System.out.println("Снято со счёта " + sum);
+            System.out.println("Остаток на счёта " + (amount = amount - sum));
+        }
+    }
+        /*if (getAmount() < sum) {
             throw new LimitException("У Вас недостаточно средств", getAmount());
         }
         amount = amount - sum;
         System.out.println("C вашего счёта снято  = " + sum);
 
 
-    }
+    }*/
 
     public double deposit(double sum){
         if (sum > 0){
